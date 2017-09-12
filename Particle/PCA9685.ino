372212377dff74c8d5e18fbdbeac235cbebac53e
zaphod-42 <trey@tomadoh.com>
2017-09-12 10:42:15 -0500

    Update PCA9685.ino

--- Particle/PCA9685.ino
+++ Particle/PCA9685.ino
@@ -7,15 +7,11 @@ void setup() {
     pwm.init();
     pwm.setPWMFreq(30);
     Particle.function("channel1", channel_1);
-    Particle.function("readPWM", read_pwm);
 }
 
 int channel_1(String val){
     pwm.setChannel(0, val.toInt());
     return 1;
-}
-int read_pwm(String cmd){
-    return pwm.readChannel(cmd.toInt());
 }
 void loop() {
 
