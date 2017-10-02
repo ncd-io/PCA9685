54701c73524cceaee9ab7c42f93dbb6c38b9d53d
io-Jacob <web@controlanything.com>
2017-10-02 13:47:31 -0500

    Update pca9685.py

--- Python/pca9685.py
+++ Python/pca9685.py
@@ -14,29 +14,29 @@ class PCA9685():
             self.address = PCA9685_DEFAULT_ADDRESS
         if not hasattr(self, 'device_attenuation'):
             self.device_attenuation = PCA9685_DEVICE_ATTENUATION
-            
+
         self.smbus = smbus
         self.reset()
-        
+
     def set_pwm_frequency(self, frequency_in_hertz):
         prescaleval = 25000000
         prescaleval /= 4096
         prescaleval /= (frequency_in_hertz * self.device_attenuation)
         prescaleval -= 1
         prescale = prescaleval + 0.5
-        
-        old_mode = self.smbus.read_byte_data(self.address, 0x00)        
-        
+
+        old_mode = self.smbus.read_byte_data(self.address, 0x00)
+
         new_mode = (old_mode&0x7F) | 0x10
         self.smbus.write_byte_data(self.address, 0x00, (old_mode&0x7F) | 0x10)
         self.smbus.write_byte_data(self.address, 0xFE, int(prescale))
         self.smbus.write_byte_data(self.address, 0x00, old_mode)
         time.sleep(.005)
         self.smbus.write_byte_data(self.address, 0x00, old_mode | 0x80)
-        
+
     def _pwm_command(self, channel, on, off):
         self.smbus.write_i2c_block_data(self.address, channel, [on, on>>8, off, off>>8])
-        
+
     def set_single_channel(self, channel, value):
         channel = 6+4*channel
         if value == 4095:
@@ -45,14 +45,14 @@ class PCA9685():
             self._pwm_command(channel,  0, 4096)
         else:
             self._pwm_command(channel, 0, value)
-            
-            
+
+
     def read_single_channel(self, channel):
         retval = self.smbus.read_i2c_block_data(self.address, 6+4*channel, 4)
         return retval
-    
+
     def set_all_channels(self, value):
         self.set_single_channel(61, value)
-        
+
     def reset(self):
         self.smbus.write_byte_data(self.address, 0x00, 0x20)
